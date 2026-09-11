2e9434ffb22c5ecbb9ef21bf969b9acf778e18c5
Sovann Reach <reachsovann063@gmail.com>
2026-09-11 12:07:57 +0700

    add

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,10 @@
+import ProfileForm from "@/components/shadcn-space/blocks/forms-01/profile-form";
+
+
+export default function ContactPage() {
+  return (
+    <div> 
+    <ProfileForm/>
+    </div>
+  )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/shadcn-space/blocks/navbar-01/navbar";
+import { Footer } from "@/components/footer7";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,7 +25,11 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+       <Navbar/>
+        {children}</body>
+        <Footer/>
+
     </html>
   );
 }
